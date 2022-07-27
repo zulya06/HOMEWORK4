abfdd526f5edf061b34a3274a4e3afb149d3dfa2
 zulya06 <kurbanova.z1990@mail.ru>
2022-07-28 01:37:05 +0300

    created task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,5 +1,7 @@
 ﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
+Console.Write("Введите число N: ");
+int numN = Convert.ToInt32(Console.ReadLine());
 
   int SumNum(int numN){
     
@@ -7,8 +9,7 @@
     int advance = 0;
     int result = 0;
 
-    for (int i = 0; i < counter; i++)
-    {
+    for (int i = 0; i < counter; i++){
       advance = numN - numN % 10;
       result = result + (numN - advance);
       numN = numN / 10;
@@ -16,7 +17,5 @@
    return result;
   }
 
-Console.Write("Введите число N: ");
-int numN = Convert.ToInt32(Console.ReadLine());
 int sumNumber = SumNum(numN);
-Console.WriteLine("Сумма цифр в числе: " + sumNumber);
+Console.WriteLine("Сумма цифр в числе: " + SumNum);
---
+++ task2/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] n= new int[8];
+      
+void Pavelprivet (int[] array) // this is joke name of metod Creatrarray
+{
+    for (int i = 0; i<array.Length; i++ )
+    {
+        array[i] = new Random().Next(-666, 666);
+    }
+}
+
+void Howeareyou (int[] array) // name of metod Pechatarray
+{
+    for (int i = 0; i<array.Length; i++ )
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine(array);
+}
+
+Pavelprivet (n);
+Howeareyou (n);
