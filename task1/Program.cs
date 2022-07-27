74ce357173158577edd48f3133876f1eb2b75513
 zulya06 <kurbanova.z1990@mail.ru>
2022-07-28 01:31:43 +0300

    created task1

---
+++ task1/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+  int SumNum(int numN){
+    
+    int counter = Convert.ToString(numN).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++)
+    {
+      advance = numN - numN % 10;
+      result = result + (numN - advance);
+      numN = numN / 10;
+    }
+   return result;
+  }
+
+Console.Write("Введите число N: ");
+int numN = Convert.ToInt32(Console.ReadLine());
+int sumNumber = SumNum(numN);
+Console.WriteLine("Сумма цифр в числе: " + sumNumber);
